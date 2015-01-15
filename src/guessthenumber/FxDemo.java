23ba648db8ebadf492b455657bae8b18d97bbce1
Solifex <solifex@gmail.com>
2015-01-15 20:57:45 +0100

    Going for full example on FxDemo

--- src/guessthenumber/FxDemo.java
+++ src/guessthenumber/FxDemo.java
@@ -1,9 +1,19 @@
 package guessthenumber;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sun.security.krb5.internal.PAData;
 import javafx.application.Application;
+import javafx.beans.property.SetProperty;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -18,19 +28,56 @@ public class FxDemo extends Application {
  * Will rewrite complete ui	
  */
 	
+	static TextArea outputTextArea;
+	static TextField inputTextField;
+	static Button sendButton;
+	
    @Override
-   public void start(final Stage primaryStage) throws Exception
+   public void start(final Stage gameStage) throws Exception
    {
-	   primaryStage.setTitle("FxDemo");
+	   gameStage.setTitle("FxDemo");
 	   
-	   BorderPane componentLayout = new BorderPane();	componentLayout.setPadding(new Insets(20,0,20,20));
 	   
-	   Scene appScene = new Scene(componentLayout,500,500);
-       primaryStage.setScene(appScene);
-       primaryStage.show();
+	   outputTextArea = new TextArea();
+	   inputTextField = new TextField();
+	   sendButton = new Button();
+	   
+	   outputTextArea.setEditable(false);
+	   outputTextArea.setFocusTraversable(false);
+	   
+	   inputTextField.isFocused();
+	   inputTextField.setOnKeyPressed(new EventHandler<Event>() {
+
+		@Override
+		public void handle(Event event) {
+			
+			String text = inputTextField.getText();
+			showText(text);
+			
+		}
+	});
+	   
+	   BorderPane bpane = new BorderPane();
+	   bpane.setPadding(new Insets(15,15,15,15));
+
+	   bpane.setCenter(outputTextArea);
+	   bpane.setBottom(inputTextField);
+	   //bpane.setBottom(sendButton);
+	   
+	   Scene gameScene = new Scene(bpane,500,500);
+	  
+	   
+       gameStage.setScene(gameScene);
+       gameStage.show();
 	   
    }
+   
+   public static void showText(String text) {
+	   outputTextArea.appendText(text + "\n");
+   }
 
+ 
+   
    /**
     * Main JavaFX application launching method.
     * 
@@ -39,5 +86,9 @@ public class FxDemo extends Application {
    public static void main(final String[] arguments)
    {
       Application.launch(arguments);
+      
+      showText("This is some test text");
    }
+
+
 }
--- src/guessthenumber/GuessTheNumber.java
+++ src/guessthenumber/GuessTheNumber.java
@@ -1,37 +1,37 @@
 package guessthenumber;
-
+ 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+import java.util.Scanner; 
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
-
-
+ 
+ 
+ 
 public class GuessTheNumber implements ActionListener {
 	
-	public static JTextField inputTextField;
-	public static JTextArea outputJTextArea;
-
+	    Scanner Scanner = new Scanner(System.in);
+	    
+        public static int number;
+        public int inputNumber;
+        public String inputString;
+        public static JTextField inputTextField;
+        public static JTextArea outputJTextArea;
+ 
 public GuessTheNumber(){
   inputTextField  = new JTextField();
   inputTextField.setSize(500, 25);
   inputTextField.setText("");
   inputTextField.setLocation(0, 450);
-  inputTextField.addActionListener(this);
-
-    
+  inputTextField.addActionListener(this);    
+     
   outputJTextArea = new JTextArea();
   outputJTextArea.setSize(500, 475);
   outputJTextArea.setRows(20);
-  outputJTextArea.setEditable(false); 
-  
-  JButton randomButton = new JButton();
-  randomButton.setText("random Number");
-  randomButton.setSize(200, 28);
+  outputJTextArea.setEditable(false);
               
   JFrame frame = new JFrame();
      frame.setDefaultCloseOperation(3);
@@ -40,50 +40,89 @@ public GuessTheNumber(){
      frame.setLocation(500, 0);
      frame.setResizable(false);
      frame.add(inputTextField);
-     frame.add(outputJTextArea);     
-  
- 
-             
+     frame.add(outputJTextArea);    
+	 
  }
-
-private static void appendToTextArea()
+ 
+public static void appendToTextArea()
 {
-	int number = randomNumber(100);
-	outputJTextArea.append("random number " + number);
-
-}
-
-
-public void actionPerformed(ActionEvent evt) {
-    String text = inputTextField.getText();
-    outputJTextArea.append(text + "\n");
-    inputTextField.selectAll();
-
-    
-    outputJTextArea.setCaretPosition(outputJTextArea.getDocument().getLength());
+        number = randomNumber(100);
+        outputJTextArea.append("random number " + number + "\n");
+ 
 }
  
- public static int randomNumber(int x) {
-	 	Random rand = new Random();
-	 	int numberToGuess = rand.nextInt(x);
-	  
-	 	System.out.print(numberToGuess);
-	 	return numberToGuess;
-	 	
+ 
+public void actionPerformed(ActionEvent evt) {
+    inputString = inputTextField.getText();
+
+	checkNumber();
+//	inputNumber = Scanner.nextInt();
+	
+//	while(number!=inputNumber){
+//		if(number>inputNumber){
+//			outputJTextArea.append("you guessed to low try again");
+//		}
+//		else if (number<inputNumber) {
+//			outputJTextArea.append("you guessed to high try again");
+//		}
+//		else{outputJTextArea.append("you guessed correct. The right number was " + number);
+//     	}	
+//	}            
+}
+
+private void checkNumber() {
+	try {
+		Integer.parseInt(inputString);
+	}
+	catch (NumberFormatException e) {
+		outputJTextArea.append("Only number are valid entries" + "\n");
+		inputTextField.setText("Enter a number");
+		inputTextField.selectAll();
+	}
+	
+	inputNumber = Integer.parseInt(inputString);
+	
+	guessThis();
+	
+}
+ 
+private void guessThis() {
+
+	if (inputNumber > number) {
+		outputJTextArea.append("Guessed high" + "\n");
+	}
+	else if (inputNumber < number) {
+		outputJTextArea.append("Guessed low" + "\n");
+	}
+	else {
+		outputJTextArea.append("You have guessed right" + "\n");
+	}
+	
+	
+}
+private static int randomNumber(int x) {
+                Random rand = new Random();
+                int numberToGuess = rand.nextInt(x);
+         
+                System.out.print(numberToGuess);
+                return numberToGuess;
 }
  
  
  public static void main(String[] args) {
-  
- new GuessTheNumber();
- 
- appendToTextArea();
 
- 
- }
-
-
- 
- 
-
+     javax.swing.SwingUtilities.invokeLater(new Runnable() {
+         public void run() {
+        	 new GuessTheNumber();
+        	 
+        	 appendToTextArea();
+        	 
+        	 inputTextField.requestFocusInWindow();
+         }
+     });
+	 
+     
+     
+      
+ } 
 }
